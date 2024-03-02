e5a164ddb95cc757d967b6568feaa23b5e8de7c4
hidden.sloth16@gmail.com <hidden.sloth16@gmail.com>
2024-03-02 14:50:11 +0200

    Cleared pipeline and Program.cs

---
+++ PharMedTOGO/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using PharMedTOGO.Data;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection service)
+        {
+            return service;
+        }
+
+        public static IServiceCollection AddApplicationDbContext(this IServiceCollection service, IConfiguration config)
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            service.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            service.AddDatabaseDeveloperPageExceptionFilter();
+
+            return service;
+        }
+
+        public static IServiceCollection AddApplicationIdentity(this IServiceCollection service)
+        {
+            service.AddDefaultIdentity<IdentityUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = true;
+
+            }).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return service;
+        }
+    }
+}
