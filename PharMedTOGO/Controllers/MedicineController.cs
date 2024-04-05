19bea2437dbd21fbcf0cd8899633400e5fdc85df
hidden.sloth16@gmail.com <hidden.sloth16@gmail.com>
2024-04-06 00:15:57 +0300

    Added MedicineController and view for the Adding functionality

---
+++ PharMedTOGO/Controllers/MedicineController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PharMedTOGO.Core.Models;
+
+namespace PharMedTOGO.Controllers
+{
+    public class MedicineController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View(new MedicineFormModel());
+        }
+    }
+}
